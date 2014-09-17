3eebf347cd448b53d7971bffec0144dc825bb2f4
John Otander <johnotander@gmail.com>
2014-09-17 17:39:28 -0600

    Add scss gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,21 @@
 var gulp    = require('gulp');
+var sass    = require('gulp-sass');
+var cssmin  = require('gulp-minify-css');
+var csslint = require('gulp-csslint');
+var prefix  = require('gulp-autoprefixer');
 var concat  = require('gulp-concat');
 var rename  = require('gulp-rename');
 var jshint  = require('gulp-jshint');
 var uglify  = require('gulp-uglify');
 
 gulp.task('scss', function() {
-  return gulp.src('src/scss/basscss/basscss.scss')
+  return gulp.src('public/scss/all.scss')
     .pipe(sass())
     .pipe(prefix("last 1 version", "> 1%", "ie 8"))
     .pipe(csslint())
     .pipe(cssmin())
     .pipe(rename('c.min.css'))
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('public'));
 });
 
 gulp.task('js', function() {
@@ -22,4 +26,4 @@ gulp.task('js', function() {
     .pipe(gulp.dest('public/js'))
 })
 
-gulp.task('default', ['js']);
+gulp.task('default', ['js', 'scss']);
